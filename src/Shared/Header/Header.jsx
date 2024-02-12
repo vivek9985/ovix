2d3d2a4a961da9936525002193bcae56a0ea5b04
vivek9985 <vivekanando363@gmail.com>
2024-02-12 20:38:13 +0600

    added animation

--- src/Shared/Header/Header.jsx
+++ src/Shared/Header/Header.jsx
@@ -7,18 +7,14 @@ import { MdOutlineClose } from "react-icons/md";
 const navItems = [
   {
     route: "Home",
-    link: "/",
-    delay: 0.4,
   },
   {
     route: "Pages",
     link: "/",
-    delay: 0.5,
   },
   {
     route: "Portfolio",
     link: "/",
-    delay: 0.6,
   },
   {
     route: "Shop",
@@ -34,6 +30,39 @@ const navItems = [
   },
 ];
 
+const contactItmes = [
+  {
+    title: "Brief us",
+    para_one: "hello@ovix.com",
+    para_two: "Tel. +53 325 356 623",
+    socials: [],
+  },
+  {
+    title: "Our Office",
+    para_one: "72 Oceanview Drive Los Angeles,",
+    para_two: "CA 90045 USA",
+    socials: [],
+  },
+  {
+    title: "Follow us",
+    para_one: "",
+    para_two: "",
+    socials: [
+      {
+        route: "Behance",
+        link: "/",
+      },
+      {
+        route: "Dribble",
+        link: "/",
+      },
+      {
+        route: "Instagram",
+        link: "/",
+      },
+    ],
+  },
+];
 const Header = () => {
   const [show, setShow] = useState(false);
 
@@ -55,14 +84,22 @@ const Header = () => {
       <div className="menu-right-content">
         <button className="download-btn">DOWNLOAD CV</button>
       </div>
-      <ul className={`nav-items ${show ? "show" : "hide"}`}>
-        {navItems.map((item) => (
+
+      {/* dropdown menu */}
+      <ul className={`nav-items ${show ? "show show-nav-info" : "hide"}`}>
+        {navItems.map((item, index) => (
           <li key={item?.route}>
             <Link
               to={item?.link}
-              className={`link ${show ? "fade-up" : "fade-down"}`}
+              className={`link ${item?.class} ${
+                show ? "fade-up" : "fade-down"
+              }`}
               style={{
-                "transition-delay": ` ${item?.delay}`,
+                animationDelay: `${
+                  show
+                    ? `${0.3 + index * 0.1}s`
+                    : `${(navItems.length - index - 1) * 0.1 + 0.3}s`
+                }`,
               }}
             >
               {item?.route}{" "}
@@ -74,24 +111,46 @@ const Header = () => {
         ))}
       </ul>
       <div
-        className={`contact-info ${show ? "show show-contact-info" : "hide"}`}
+        className={`contact-info-container ${
+          show ? "show show-contact-info" : "hide"
+        }`}
       >
-        <div
-          className="close-btn"
-          onClick={() => {
-            setShow(false);
-          }}
-        >
-          <MdOutlineClose />
+        <div className="close-btn">
+          <MdOutlineClose className="close-icon"
+            onClick={() => {
+              setShow(false);
+            }}
+          />
         </div>
-        <h2>Brief us</h2>
-        <p>hello@ovix.com</p>
-        <p>Tel. +53 325 356 623</p>
-        <h2>Our Office</h2>
-        <p>72 Oceanview Drive Los Angeles,</p>
-        <p>CA 90045 USA</p>
-        <h2>Follow us</h2>
-        <p>Behance Dribble Instagram</p>
+        <ul>
+          {contactItmes.map((item, index) => (
+            <li
+              key={index}
+              className={`contact-info-content ${show ? "visible" : "hidden"}`}
+              style={{
+                animationDelay: `${
+                  show
+                    ? `${0.3 + index * 0.1}s`
+                    : `${(navItems.length - index - 1) * 0.1 + 0.3}s`
+                }`,
+              }}
+            >
+              <div>
+                <h2>{item?.title}</h2>
+                <p>{item?.para_one}</p>
+                <p>{item?.para_two}</p>
+                <ul className="socials">
+                  {item?.socials.map((subItem) => (
+                    <li>
+                      <FaArrowRight className="arrow" />
+                      <Link className="social-links">{subItem?.route}</Link>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </li>
+          ))}
+        </ul>
       </div>
     </header>
   );
